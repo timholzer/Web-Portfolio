cd2b587e921287c375c9fbdba79042bb77f73805
Tim Holzer <timiholzer@gmail.com>
2020-09-04 23:22:08 -0600

    added dark, darker, and darkest modes

---
+++ js/modes.js
@@ -0,0 +1,16 @@
+document.getElementById("normalmode").addEventListener("click", function(){
+    var stylesheet = document.getElementById('stylesheet');
+    stylesheet.href = "css/homestyle.css";
+});
+document.getElementById("darkmode").addEventListener("click", function(){
+    var stylesheet = document.getElementById('stylesheet');
+    stylesheet.href = "css/darkmode.css";
+});
+document.getElementById("darkermode").addEventListener("click", function(){
+    var stylesheet = document.getElementById('stylesheet');
+    stylesheet.href = "css/darkermode.css";
+});
+document.getElementById("darkestmode").addEventListener("click", function(){
+    var stylesheet = document.getElementById('stylesheet');
+    stylesheet.href = "css/darkestmode.css";
+});
